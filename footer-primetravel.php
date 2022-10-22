0ee76c6b51b886b54125a0f3640aedfda2fefb31
Chris Parsons <chris@parsonshosting.com>
2022-10-21 20:14:18 -0700

    New for prime travel template

---
+++ footer-primetravel.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * The template for displaying the footer.
+	 *
+	 * Contains the closing of the #content div and all content after.
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+	 *
+	 * @package The_Fly_Shop
+	 */
+
+?>
+<!-- Footer -->
+<footer id="footer">
+	<!-- ========= INSTAGRAM FEED ========== -->
+	<div class="container-fluid">
+	</div>
+</footer>
+<?php
+	if ( is_front_page() ) : ?>
+		</div>
+	<?php
+	endif;
+	
+	wp_footer(); ?>
+</div>
+</body>
+</html>
