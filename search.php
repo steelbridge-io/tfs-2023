fea8f21b93f758ee88072b39167a921f1d23287c
Chris Parsons <chris@parsonshosting.com>
2022-12-22 12:32:03 -0800

    Added search bar to search.php

--- search.php
+++ search.php
@@ -16,38 +16,42 @@ get_header(); ?>
       
       <div id="search-container-row" class="row">
         <div class="col-md-8">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header search-results">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-fly-shop' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			//the_posts_navigation();
-			wpbeginner_numeric_posts_nav();
+				<main id="main" class="site-main" role="main">
+				
+				<?php
+				if ( have_posts() ) : ?>
 		
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
+					<header class="page-header search-results">
+						<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'the-fly-shop' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					</header><!-- .page-header -->
+					
+					<div id="search-bar-search-page">
+					<?php get_search_form(); ?>
+					</div>
+		
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+		
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
+		
+					endwhile;
+		
+					//the_posts_navigation();
+					wpbeginner_numeric_posts_nav();
+				
+				else :
+		
+					get_template_part( 'template-parts/content', 'none' );
+		
+				endif; ?>
+		
+				</main><!-- #main -->
       </div>
         <div class="col-md-3 col-md-offset-1">
           <?php get_sidebar(); ?>
