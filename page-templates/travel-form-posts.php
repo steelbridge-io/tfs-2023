ea660d79d5015a2deda825033a07e9d5c79dedc3
steelbridge-io <chris@steelbridge.io>
2024-06-15 12:00:49 -0700

    Add new fields to travel form
    New form fields for departure date, telephone number, and dietary allergies have been added to the travel form. The display of some form data has also been modified for consistency and readability. Corresponding filters and search criteria for these fields have also been set up.

--- page-templates/travel-form-posts.php
+++ page-templates/travel-form-posts.php
@@ -31,12 +31,25 @@ endif;
 echo '<div id="filter-cont" class="container filter-wrap">';
 echo '<form method="GET">';
 
+/** @var  $arrival_date */
+
 $arrival_date = filter_input(INPUT_GET, 'filter_arrival_date', FILTER_SANITIZE_STRING);
 
 echo '<label for="filter_arrival_date">Arrival Date:</label>';
 echo '<input type="date" id="filter_arrival_date" name="filter_arrival_date" value="'
      . (isset($arrival_date) ? $arrival_date : '')
      . '">';
+
+/** @var  $departure_date */
+
+$departure_date = filter_input(INPUT_GET, 'filter_departure_date', FILTER_SANITIZE_STRING);
+
+echo '<label for="filter_departure_date">Departure Date:</label>';
+echo '<input type="date" id="filter_departure_date" name="filter_departure_date" value="'
+     . (isset($departure_date) ? $departure_date : '')
+     . '">';
+
+/** @var  $name */
 
 $name = filter_input(INPUT_GET, 'filter_name', FILTER_SANITIZE_STRING);
 
@@ -46,18 +59,38 @@ echo '<input type="text" id="filter_name" name="filter_name" value="'
      . '">';
 
 echo '<div id="question-items" class="display-items-inline">';
+
+/** @var  $tel_number */
+
+$tel_number = filter_input(INPUT_GET, 'filter_tel_number', FILTER_SANITIZE_STRING);
+
+echo '<label for="filter_tel_number">Tel:</label>';
+echo '<input class="no-margin" type="checkbox" id="filter_tel_number" name="filter_tel_number" value="Yes"'
+     . (isset($tel_number) && $tel_number == 'Yes' ? ' checked' : '') . '>';
+
+/** @var  $shuttle_service */
+
 $shuttle_service = filter_input(INPUT_GET, 'filter_shuttle_service', FILTER_SANITIZE_STRING);
 
 echo '<label for="filter_shuttle_service">Shuttle Service:</label>';
 echo '<input class="no-margin" type="checkbox" id="filter_shuttle_service" name="filter_shuttle_service" value="Yes"'
      . (isset($shuttle_service) && $shuttle_service == 'Yes' ? ' checked' : '') . '>';
 
+/** @var  $other_travelers */
 
 $other_travelers = filter_input(INPUT_GET, 'filter_other_travelers', FILTER_SANITIZE_STRING);
 
 echo '<label for="filter_other_travelers">Other Travelers:</label>';
 echo '<input class="no-margin" type="checkbox" id="filter_other_travelers" name="filter_other_travelers" value="Yes"'
      . (isset($other_travelers) && $other_travelers == 'Yes' ? ' checked' : '') . '>';
+
+/** @var  $dietary_allergies */
+
+$dietary_allergies = filter_input(INPUT_GET, 'filter_dietary_allergies', FILTER_SANITIZE_STRING);
+
+echo '<label for="filter_dietary_allergies">Allergies &amp; Dietary:</label>';
+echo '<input class="no-margin" type="checkbox" id="filter_dietary_allergies" name="filter_dietary_allergies" value="Yes"'
+     . (isset($dietary_allergies) && $dietary_allergies == 'Yes' ? ' checked' : '') . '>';
 
 echo '</div>';
 
@@ -76,6 +109,10 @@ if (isset($arrival_date) && $arrival_date != '') {
 	$search_criteria['field_filters'][] = array('key' => '22', 'value' => $arrival_date); // check arrival date
 }
 
+if (isset($departure_date) && $departure_date != '') {
+	$search_criteria['field_filters'][] = array('key' => '23', 'value' => $departure_date); // check arrival date
+}
+
 if (isset($_GET['filter_name']) && $_GET['filter_name'] != '') {
 	$search_criteria['field_filters'][] = array('key' => '1.6', 'value' => $_GET['filter_name']); // check name
 }
@@ -88,16 +125,21 @@ if ($other_travelers == 'Yes') {
 	$search_criteria['field_filters'][] = array('key' => '21', 'operator' => 'isnot', 'value' => '');
 }
 
+if ($dietary_allergies == 'Yes') {
+	$search_criteria['field_filters'][] = array('key' => '37', 'operator' => 'isnot', 'value' => '');
+}
 
-/* end shuttle service */
+if ($tel_number == 'Yes') {
+	$search_criteria['field_filters'][] = array('key' => '26', 'operator' => 'isnot', 'value' => '');
+}
 
 echo '<div class="container form-list-wrap">';
 
 $form_id                   = '16'; // Your form ID
 $search_criteria['status'] = 'active';
 $sorting                   = array(
-	'key'        => '1.3',
-	'direction'  => 'DESC',
+	'key'        => '1.6',
+	'direction'  => 'ASC',
 	'is_numeric' => FALSE,
 ); // 1.3 is the field id for last name
 
@@ -110,23 +152,40 @@ foreach ( $entries as $entry ) {
 	echo '<div class="container destination-form">';
 	echo '<div class="row">';
 	
-	echo '<div class="col-12 name-fml form-entry"><b>First Name:</b><span class="name-g">'
-	     . rgar( $entry, '1.3' ) . '</span><b>Middle:</b><span class="name-g">'
-	     . rgar( $entry, '1.4' )
-	     . '</span><b>Last Name:</b><span class="name-g">' . rgar( $entry,
-			'1.6' ) . '</span></div>';
+	echo '<div class="col-12 name-fml form-entry"><b>Last Name:</b><span class="name-g">'
+	     . rgar( $entry, '1.6' ) . '</span><b>First Name:</b><span class="name-g">'
+	     . rgar( $entry, '1.3' )
+	     . '</span><b>Middle Name:</b><span class="name-g">' . rgar( $entry,
+			'1.4' ) . '</span></div>';
 	
+	echo '<div class="col-12 form-entry"><b>Date of arrival:</b> '
+	     . rgar( $entry, '22' ) . '</div>';
+	
+	echo '<div class="col-12 form-entry"><b>Date of departure:</b> '
+	     . rgar( $entry, '23' ) . '</div>';
+	
+	if (isset($tel_number) && $tel_number == 'Yes') {
+		echo '<div class="col-12 form-entry"><b>Tel:</b>'
+		     . rgar( $entry, '26' ) . '</div>';
+	}
+ 
 	if (isset($other_travelers) && $other_travelers == 'Yes') {
 		echo '<div class="col-12 form-entry"><b>Names of others traveling with guest:</b><br>'
 		     . rgar( $entry, '21' ) . '</div>';
 	}
 	
-	if (isset($shuttle_service)) {
+	if (isset($shuttle_service) && $shuttle_service == 'Yes') {
+	//if (isset($shuttle_service)) {
 		echo '<div class="col-12 form-entry"><b>Do you need Shuttle service?</b><br>'
 		     . rgar( $entry, '34' ) . '</div>';
 		
 		echo '<div class="col-12 form-entry"><b>If you are in need of a shuttle, please let us know location of pick up either Jackson or Idaho Falls and put your arrival/departure flight numbers and arrival/departure times below. This service is extra and not included in the price of the trip:</b><br>'
 		     . rgar( $entry, '35' ) . '</div>';
+	}
+	
+	if (isset($dietary_allergies) && $dietary_allergies == 'Yes') {
+		echo '<div class="col-12 form-entry"><b>Please list any allergies and/or dietary restrictions here:</b><br>'
+		     . rgar( $entry, '37' ) . '</div>';
 	}
  
 	?>
@@ -143,18 +202,15 @@ foreach ( $entries as $entry ) {
 	
 	<?php
 	
-	echo '<div class="col-12 form-entry"><b>Names of others traveling with guest:</b><br>'
-	     . rgar( $entry, '21' ) . '</div>';
-	
-	echo '<div class="col-12 form-entry"><b>Date of arrival:</b> '
-	     . rgar( $entry, '22' ) . '</div>';
-	echo '<div class="col-12 form-entry"><b>Date of departure:</b> '
-	     . rgar( $entry, '23' ) . '</div>';
 	echo '<div class="col-12 form-entry"><b>Date of birth:</b>' . rgar( $entry,
 			'24' ) . '</div>';
-	echo '<div class="col-12 form-entry"><b>Preferred contact tel:</b>'
+	
+	echo '<div class="col-12 form-entry"><b>Contact tel:</b>'
 	     . rgar( $entry, '26' ) . '</div>';
 	
+	echo '<div class="col-12 form-entry"><b>Names of others traveling with guest:</b><br>'
+	     . rgar( $entry, '21' ) . '</div>';
+    
 	echo '<div class="title-desc form-entry"><b>Emergency Contact Person (Name):</b><br><b>First name:</b><span class="name-g">'
 	     . rgar( $entry, '28.3' )
 	     . '</span><b>Last name:</b><span class="name-g">' . rgar( $entry,
