7e13a9f50eb8800452e61dc30771df611785704c
steelbridge-io <chris@steelbridge.io>
2024-07-17 20:12:10 -0700

    Update body class concatenation in header.php
    The body class concatenation method has been updated in the header.php file. Instead of using '&nbsp;' to concatenate the class names, it now correctly uses ' ' (a single space). This adjustment should facilitate improved HTML readability and ensure proper CSS class application.

--- header.php
+++ header.php
@@ -55,7 +55,7 @@ $tfs_metatags    = get_post_meta(get_the_ID(), 'seotfs-meta-tags', true);
 
 <!--<body class="landing">-->
 
-<?php echo '<body class="scroll landing archivetemplate sections-template stream-report travel-destination outfitters-dept private-waters schools-template fish-camp-template default-template non-banner-loading&nbsp;'.implode('&nbsp;', get_body_class()).'">'.PHP_EOL; ?>
+<?php echo '<body class="scroll landing archivetemplate sections-template stream-report travel-destination outfitters-dept private-waters schools-template fish-camp-template default-template non-banner-loading '.implode(' ', get_body_class()).'">'.PHP_EOL; ?>
 
 <div class="container-fluid">
   
