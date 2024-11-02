544b23fdb8e58b46db9859fdc0566e15b4e4fc76
steelbridge-io <chris@steelbridge.io>
2024-11-01 20:10:45 -0700

    Optimize and clean up travel form posts template
    Refactored and streamlined the `travel-form-posts-v2.php` template for better readability and maintainability. Removed commented-out code, improved indentation, and standardized the way some functions and HTML elements are written.

--- questionnaire-templates/travel-form-posts-v2.php
+++ questionnaire-templates/travel-form-posts-v2.php
@@ -12,35 +12,25 @@
 get_header();
 
 echo '<div id="travel-form-posts" class="container-fluid">';
-
 echo '<div class="container"><h1>' . get_the_title() . '</h1></div>';
 
-if ( have_posts() ) :
-    while ( have_posts() ) :
-        the_post();
-        echo '<div class="post-content container">';
-        the_content();
-        echo '</div>';
-    endwhile;
+if (have_posts()) :
+	while (have_posts()) : the_post();
+		echo '<div class="post-content container">';
+		the_content();
+		echo '</div>';
+	endwhile;
 else :
-    echo '<p>';
-    _e( 'Sorry, no posts matched your criteria.' );
-    echo '</p>';
+	echo '<p>' . __('Sorry, no posts matched your criteria.') . '</p>';
 endif;
 
 /**
  * Open Reveal
  */
-/*echo '<div class="container gda-filter-wrapper">
-        <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Filter Table
-        </button>
-      </div>';*/
-
 echo '<div class="container gda-search-wrapper">
         <div class="row display-flex align-items-center">
             <div class="col-md-4">
-            <input type="text" id="searchInput" placeholder="Search table..">
+                <input type="text" id="searchInput" placeholder="Search table..">
             </div>
             <div class="col-md-4">
                 <div class="search-buttons">
@@ -51,134 +41,89 @@ echo '<div class="container gda-search-wrapper">
             </div>
             <div class="col-md-4">
                 <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Filter Table
+                    Filter Table
                 </button>
             </div>
-         </div>
+        </div>
       </div>';
 
 echo '<div class="collapse" id="collapseExample">';
 
-/**
- * The form_id variable is used to store the unique identifier of a form.
- *
- * @var string $form_id The unique identifier of the Gravity Form.
- */
-//$form_id                   = '59'; // Your Gravity Form ID
-
+// Retrieve form ID based on the guest number
 $guest_number = get_post_meta(get_the_ID(), '_gda_meta_key', true);
 if ($guest_number) {
-  $form_id = ($guest_number);
+	$form_id = ($guest_number);
 }
 
 $search_criteria['status'] = 'active';
 
 // Sorting based on "Arrival date" field with ID 46 in descending order
-$sorting = array('key' => '46', // Field ID for "Arrival date"
-  'direction' => 'ASC', // Sorting in descending order
-  'is_numeric' => FALSE,);
+$sorting = array(
+	'key' => '46', // Field ID for "Arrival date"
+	'direction' => 'ASC', // Sorting in descending order
+	'is_numeric' => false,
+);
 
 if ($form_id == 96) {
-  require_once get_template_directory() . '/questionnaire-config/search/alaska-rainbow-adventures-search.php';
+	require_once get_template_directory() . '/questionnaire-config/search/alaska-rainbow-adventures-search.php';
 }
 if ($form_id == 95) {
-  require_once get_template_directory() . '/questionnaire-config/search/alaska-steelhead-co-search.php';
+	require_once get_template_directory() . '/questionnaire-config/search/alaska-steelhead-co-search.php';
 }
 if ($form_id == 85) {
-  require_once get_template_directory() . '/questionnaire-config/search/epic-alaska-search.php';
+	require_once get_template_directory() . '/questionnaire-config/search/epic-alaska-search.php';
 }
 
 /**
  * Close reveal
  */
 echo '</div>';
-
-echo '<div class="container form-list-wrap">';
-
-echo '</div>';
+echo '<div class="container form-list-wrap"></div>';
 
 if ($form_id == 96) {
-  include_once(get_template_directory() . '/questionnaire-config/table-headings/alaska-rainbow-adventures-table.php');
+	include_once(get_template_directory() . '/questionnaire-config/table-headings/alaska-rainbow-adventures-table.php');
 }
 if ($form_id == 95) {
-  require_once get_template_directory() . '/questionnaire-config/table-headings/alaska-steelhead-co-table.php';
+	require_once get_template_directory() . '/questionnaire-config/table-headings/alaska-steelhead-co-table.php';
 }
 if ($form_id == 85) {
-  require_once get_template_directory() . '/questionnaire-config/table-headings/epic-alaska-table.php';
+	require_once get_template_directory() . '/questionnaire-config/table-headings/epic-alaska-table.php';
 }
 
-/*
+/**
  * Retrieves entries from a Gravity Form based on specified criteria and sorting order
  * $form_id: ID of the form to retrieve entries from
  * $search_criteria: Array to filter entries (e.g., status, date range, field values)
  * $sorting: Array to define the sorting order of entries (e.g., by date created, direction)
-**/
+ */
 $entries = GFAPI::get_entries($form_id, $search_criteria, $sorting);
 
 if ($form_id == 59) {
-  require_once get_template_directory() . '/questionnaire-config/question-config.php';
+	require_once get_template_directory() . '/questionnaire-config/question-config.php';
 }
 if ($form_id == 96) {
-  require_once get_template_directory() . '/questionnaire-config/questionnaire/alaska-rainbow-adventures-ak-questionnaire.php';
+	require_once get_template_directory() . '/questionnaire-config/questionnaire/alaska/alaska-rainbow-adventures-ak-questionnaire.php';
 }
 if ($form_id == 95) {
-  require_once get_template_directory() . '/questionnaire-config/questionnaire/alaska-steelhead-co-questionnaire.php';
+	require_once get_template_directory() . '/questionnaire-config/questionnaire/alaska/alaska-steelhead-co-questionnaire.php';
 }
 if ($form_id == 85) {
-  require_once get_template_directory() . '/questionnaire-config/questionnaire/epic-alaska-questionnaire.php';
+	require_once get_template_directory() . '/questionnaire-config/questionnaire/epic-alaska-questionnaire.php';
 }
 
 foreach ($entries as $entry) {
-  formatEntryData($entry, $counter);
+	formatEntryData($entry, $counter);
 }
 
 echo '</tbody>'; // end tbody
 echo '</table>'; // end table
 echo '</div>'; // end table-scrollable
 echo '</div>'; // end table-wrapper
-
 echo '</div>'; // end travel-form-posts div
-?>
-    <script>
-        jQuery(document).ready(function(){
-        // Initialize the popover
-            jQuery('.btn-popover').popover();
-            
-            // Toggle popover on button click
-            jQuery('.btn-popover').on('click', function (e) {
-            e.stopPropagation();
-            var $this = jQuery(this);
-            // Hide other popovers
-            jQuery('.btn-popover').not($this).popover('hide');
-            // Toggle the clicked popover
-            $this.popover('toggle');
-            // Update the popover position after toggle
-            setTimeout(function () {
-            $this.popover('update');
-            }, 1);
-        });
-        
-           // Close popover when clicking outside
-           jQuery(document).on('click', function (e) {
-           var target = jQuery(e.target);
-           if (!target.closest('.popover').length && !target.closest('.btn-popover').length) {
-           jQuery('.btn-popover').popover('hide');
-            }
-        });
 
-            function showFullContent(link) {
-                var content = `{$escaped_content}`;
-                var popoverTrigger = $(link).closest('.popover').prev('.btn-popover');
-                popoverTrigger.popover('dispose'); // Dispose current popover
-                popoverTrigger.popover({
-                    content: content,
-                    html: true
-                }).popover('show'); // Show the updated popover with full content
-            }
-        });
-    
-    </script>
+?>
+
+
 
 <?php
-
 get_footer();
